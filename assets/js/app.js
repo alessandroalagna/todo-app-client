6e13dbe34b0c0bb629354b040392bd052000f3c6
alessandroalagna <alagnalessandro3@gmail.com>
2018-06-19 14:24:00 +0200

    esercizio

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,25 @@
 
     // Please read me! https://stackoverflow.com/a/8835458
     window.addEventListener('load', function() {
+        var form = document.querySelector("#form");
+        form.addEventListener('submit',function(e){
+            e.preventDefault();
+            var val = document.querySelector("#input").value;
+            console.log(val);
+            var richiesta = {
+                data: {
+                    uuid: window.app.Session.get(),
+                    content: val,
+                }
+            }
+            console.log(richiesta);
+
+            window.app.Store.createTodo(richiesta);
+        });
+
+    window.app.Store.getTodos();
+
+    
 
     });
 })(window, document);
--- assets/js/store.js
+++ assets/js/store.js
@@ -3,18 +3,45 @@
 (function(window, session) {
     // var sessionUuid = session.get();
 
-    function updateTodo(id, todoModel, callbackFunction) {
+    function call(id, todoModel, callbackFunction) {
+        var xthpp = new XMLHttpRequest();
+        
         // ajax call to post method
     }
 
-    function createTodo(todoModel, callbackFunction) {
-        // ajax call to post method
+    function createTodo(creazionetodo, callbackFunction) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://192.168.20.173:7000/todos", true);
+        xhr.setRequestHeader("Content-type", "application/json");
+        xhr.addEventListener('load', function (x) {
+                
+            var risposta=JSON.parse(x.target.response);
+            console.log(risposta);
+         });
+
+        xhr.addEventListener('error', function (x) {
+            console.log('error', x);
+        });
+
+        xhr.send(JSON.stringify(creazionetodo));
+
     }
 
     function getTodos(callbackFunction) {
-        // ajax get call
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET","http://192.168.20.173:7000/todos",true);
+        xhr.setRequestHeader("Content-type", "application/json");
+        xhr.addEventListener('load',function(x){
+
+            var risposta=JSON.parse(x.target.response);
+            console.log(risposta);
+
+        });
+        xhr.send();
+
     }
 
+    function updateTodo(){}
     // Export to window
     window.app = window.app || {};
     window.app.Store = {
