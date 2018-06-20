c9766aab8693569f815d59212e68a9d1727bc357
alessandroalagna <alagnalessandro3@gmail.com>
2018-06-20 13:42:50 +0200

    esercizio 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,12 +17,88 @@
             }
             console.log(richiesta);
 
-            window.app.Store.createTodo(richiesta);
+            window.app.Store.createTodo(richiesta,function(response){
+                console.log(response);
+                CreateElement(response.data);
+            });
+            
         });
 
-    window.app.Store.getTodos();
+        window.app.Store.getTodos(callGetTodos);
 
+        function callGetTodos(json,index){
+            console.log(json);
+            for(var i=0;i<json.data.docs.length;i++){
+                CreateElement(json.data.docs[i]);
+                console.log(json);
+            }
+        }
+
+        function CreateElement(todo){
+        
+            var ul = document.querySelector("#lista");
+            var li = document.createElement("li");
+            var hidden = document.createElement("input");
+            var checkbox = document.createElement("input");
+            var label = document.createElement("label");
+            var button = document.createElement("button");
     
+            hidden.setAttribute("type","hidden");
+            hidden.setAttribute("value",todo._id);
+    
+            checkbox.setAttribute("type","checkbox");
+            checkbox.checked = todo.done;
+            checkbox.addEventListener('change',function(e){
+                console.log(e.target.checked);
+                var richiesta= {
+                    data: {
+                        "done": e.target.checked
+                    }
+                }
 
+                window.app.Store.updateTodo(richiesta,todo._id);
+    
+            })
+            
+            var testoGras = todo.content;
+            label.textContent = testoGras.toUpperCase();
+            checkbox.classList.add("checkbox");
+            button.textContent = "CANCELLA";
+            button.classList.add("button");
+            button.addEventListener("click",function(e){
+                window.app.Store.deleteTodo(todo._id);
+                ul.removeChild(li);
+            })
+            
+    
+            li.appendChild(hidden);
+            label.appendChild(checkbox);
+            li.appendChild(label);
+            li.appendChild(button);
+            ul.appendChild(li);
+            
+        }
+
+
+    /*{
+        "data": {
+            "docs": [
+                {
+                    "_id": "5b289e57121bde36675dd4b8",
+                    "updatedAt": "2018-06-19T06:10:31.341Z",
+                    "createdAt": "2018-06-19T06:10:31.341Z",
+                    "uuid": "woeivnewiojwehvehvehve",
+                    "deleted": false,
+                    "done": true,
+                    "content": ""
+                }
+            ],
+            "total": 1,
+            "limit": 10,
+            "offset": 0,
+            "page": 1,
+            "pages": 1
+        }
+    }*/
     });
 })(window, document);
--- assets/js/store.js
+++ assets/js/store.js
@@ -1,7 +1,7 @@
 "use strict";
 
 (function(window, session) {
-    // var sessionUuid = session.get();
+    var sessionUuid = session.get();
 
     function call(id, todoModel, callbackFunction) {
         var xthpp = new XMLHttpRequest();
@@ -16,7 +16,8 @@
         xhr.addEventListener('load', function (x) {
                 
             var risposta=JSON.parse(x.target.response);
-            console.log(risposta);
+            callbackFunction(risposta);
+
          });
 
         xhr.addEventListener('error', function (x) {
@@ -29,24 +30,48 @@
 
     function getTodos(callbackFunction) {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET","http://192.168.20.173:7000/todos",true);
+        xhr.open("GET","http://192.168.20.173:7000/todos?uuid="+sessionUuid+"&limit=1000",true);
         xhr.setRequestHeader("Content-type", "application/json");
-        xhr.addEventListener('load',function(x){
-
+        xhr.addEventListener('load', function(x){
             var risposta=JSON.parse(x.target.response);
-            console.log(risposta);
-
+            callbackFunction(risposta);
         });
         xhr.send();
 
+
     }
 
-    function updateTodo(){}
-    // Export to window
+    function updateTodo(data,id){
+        var xhr = new XMLHttpRequest();
+        xhr.open("PUT","http://192.168.20.173:7000/todos/"+id,true);
+        xhr.setRequestHeader("content-type","application/json");
+        xhr.addEventListener("load",function(x){
+            console.log(x);
+        })
+
+        xhr.send(JSON.stringify(data));
+    
+    }
+
+    function deleteTodo(id){
+        var xhr = new XMLHttpRequest();
+        xhr.open("DELETE","http://192.168.20.173:7000/todos/"+id,true);
+        xhr.setRequestHeader("content-type","application/json");
+        xhr.addEventListener("load",function(x){
+            console.log(x);
+        })
+
+        xhr.send();
+    }
+     
     window.app = window.app || {};
     window.app.Store = {
         updateTodo: updateTodo,
         createTodo: createTodo,
-        getTodos: getTodos
+        getTodos: getTodos,
+        deleteTodo: deleteTodo
     };
 })(window, window.app.Session);
+
+
+
